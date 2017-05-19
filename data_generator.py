33f597753734e69325f7adfff46154960c588adf
Xiaotian Wang <wxt1994@gmail.com>
2017-05-19 12:28:20 -0500

    Added doc string for data generator

--- data_generator.py
+++ data_generator.py
@@ -7,6 +7,18 @@ class Data:
         self.Y = None
 
     def random_normal(self, size):
+        """ Generate two groups of data points from two Gaussian distributions.
+        One groupd of data points are labeled as 1s, and the other group labeled
+        as 0s.
+
+        Parameters
+        ----------
+        size: number of data points in each group
+
+        Returns
+        -------
+        N/A
+        """
         n_data = np.ones((size, 2))
         x0 = np.random.normal(2*n_data, 1)
         y0 = np.zeros(size)
